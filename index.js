791c79267c2508a438373fe0a7e83ac9e53c36a9
arsalan-smit <arsalangg05@gmail.com>
2024-10-21 18:18:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let username;
+
+let myName = "Muhammad Arsalan khan";
+
+let message = "Hello World";
+alert(message);
+
+let studentName = "Muhammad Arsalan khan";  
+let studentAge = "20";         
+let studentCourse = "Web & Mobile App Development";       
+alert(`Name: ${studentName}`);
+alert(`Age: ${studentAge}`);
+alert(`Course: ${studentCourse}`);
+
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+let email = "arsalangg05@gmail.com";
+alert(`My email address is ${email}`);
+
+let book = "A smarter way to learn JavaScript";
+alert(`I am trying to learning from the book ${book}`);
+
+document.write("Yah! i can write  a HTML content through javsscript ");
+
+let pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pattern);
+document.write(`<br>${pattern}`);
+
+
+
+
